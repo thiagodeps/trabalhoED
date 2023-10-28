de8b37e5406385d85d5c7758959bdff929fc57cc
thiago almeida deps caldeira <62445820+thiagodeps@users.noreply.github.com>
2023-10-28 17:36:01 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "image.h"
+int main()
+{
+ int rows ;
+ int cols;
+ char type[2];
+ printf("rows :");
+ scanf("%d" , &rows);
+ printf("cols :");
+ scanf("%d" , &cols);
+ printf("%d" , cols);
+}
